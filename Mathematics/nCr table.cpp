58d1c39f5c23b5891626c005cfd87c32331ff810
Masalmah <49412099+OdaMasalmah@users.noreply.github.com>
2020-10-16 18:48:35 +0300

    Create nCr table.cpp

---
+++ Mathematics/nCr table.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define int long long
+#define sz(v) (int)(v).size()
+#define mod 1000000000
+
+const int N= 1001;
+int ncr[N][N];
+
+void init() { // you can do this in k space;
+        for (int i= 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                        if (j == 0 || j == i) {
+                                ncr[i][j]= 1;
+                        } else {
+                                ncr[i][j]= ((ncr[i - 1][j - 1])%mod + (ncr[i - 1][j])%mod)% mod;
+                        }
+                }
+        }
+}
+
+void solve() {
+        int n; cin >> n;
+        for (int i= 0; i<= n; i++)
+                cout << ncr[n][i]<< " ";
+        cout << "\n";
+}
+
+int32_t main(){
+ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+        int T= 1;
+        init();
+        cin >> T;
+        while ( T-- ) {
+                solve();
+        }
+return 0;
+}
